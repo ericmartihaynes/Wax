a507cb91b066c96befc0a15e2fd46b22d11330f1
ericmartihaynes <ericmartihaynes@gmail.com>
2022-05-17 00:01:33 +0200

    Menu continued

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void Level1() {
+        SceneManager.LoadScene("Level1");
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void Level3()
+    {
         SceneManager.LoadScene("TestLevel");
     }
 
